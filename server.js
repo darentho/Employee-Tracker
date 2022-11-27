daf110f2b86349b96ae25995de015cacbe523c70
Alberto Monroy <firewind3882@gmail.com>
2022-11-27 16:41:03 -0600

    coded the server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const db = require("./db/connection");
+const apiRoutes = require("./routes/apiRoutes");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// using routes
+app.use("/api", apiRoutes);
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// database connection
+db.connect((err) => {
+  if (err) throw err;
+
+  app.listen(PORT, () => {});
+});
+
+// call init app function
+initializeApp();
